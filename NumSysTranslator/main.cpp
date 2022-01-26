abe089095979b4dfc4bb30cc32006e7e05a81d7e
mirmicuper <alexander.onthe@gmail.com>
2022-01-27 00:43:38 +0300

    Initial Commit

---
+++ NumSysTranslator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  NumSysTranslator
+//
+//  Created by Александр Гемза on 27.01.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
